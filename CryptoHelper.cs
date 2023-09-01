2466724b814d8c378378bce3d17ca45a5f9f4848
Mako864 <69087696+Mako864@users.noreply.github.com>
2023-09-02 01:43:56 +0400

    Secondary Commit (Primary Code - Crashes)

---
+++ CryptoHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+
+namespace SQLCipherDecryptor
+{
+    public class CryptoHelper
+    {
+        // SQLCipher 3.x defaults
+        const int PAGE_SIZE = 1024;
+        const int KDF_ITERATIONS = 64000;
+        const int KEY_LENGTH = 32; // 256-bit AES
+        const string HMAC_ALGORITHM = "HMACSHA1";
+        const string KDF_ALGORITHM = "HMACSHA1";
+        const int PLAINTEXT_HEADER_SIZE = 0;
+
+        public static byte[] DecryptPage(byte[] encryptedPage, byte[] encryptionKey, byte[] hmacKey, int pageNum)
+        {
+            byte[] iv = new byte[16];
+            Array.Copy(encryptedPage, encryptedPage.Length - 16, iv, 0, 16);
+
+            HMAC hmac = HMAC.Create(HMAC_ALGORITHM);
+            hmac.Key = hmacKey;
+
+            byte[] computedHmac = hmac.ComputeHash(encryptedPage, 0, encryptedPage.Length - 20);
+
+            // Verify HMAC
+            byte[] storedHmac = new byte[20];
+            Array.Copy(encryptedPage, encryptedPage.Length - 20, storedHmac, 0, 20);
+
+            if (!CompareByteArrays(computedHmac, storedHmac))
+            {
+                throw new InvalidOperationException($"HMAC verification failed for page {pageNum}.");
+            }
+
+            // Decrypt
+            Aes aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.KeySize = 256;
+            aes.Padding = PaddingMode.None;
+            aes.Key = encryptionKey;
+            aes.IV = iv;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+
+            byte[] decryptedPage = new byte[PAGE_SIZE];
+            using (MemoryStream msDecrypt = new(encryptedPage, 0, PAGE_SIZE))
+            {
+                using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
+                csDecrypt.Read(decryptedPage, 0, decryptedPage.Length);
+            }
+
+            return decryptedPage;
+        }
+
+        public static bool CompareByteArrays(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+
+namespace SQLCipherDecryptor
+{
+    class Program
+    {
+        public static async Task Decrypt(string inputFilePath, string outputFilePath, string password)
+        {
+            byte[] salt = new byte[16];
+            byte[] encryptionKey = new byte[32];
+            byte[] hmacKey = new byte[20];
+
+            // Read salt and derive keys
+            using (FileStream fs = new(inputFilePath, FileMode.Open, FileAccess.Read))
+            {
+                fs.Seek(16, SeekOrigin.Begin);
+                fs.Read(salt, 0, 16);
+            }
+
+            using (Rfc2898DeriveBytes pbkdf2 = new(System.Text.Encoding.UTF8.GetBytes(password), salt, 64000, HashAlgorithmName.SHA1))
+            {
+                byte[] masterKey = pbkdf2.GetBytes(64);
+                Array.Copy(masterKey, 0, encryptionKey, 0, 32);
+                Array.Copy(masterKey, 32, hmacKey, 0, 20);
+            }
+
+
+            using FileStream fsInput = new(inputFilePath, FileMode.Open, FileAccess.Read);
+            using FileStream fsOutput = new(outputFilePath, FileMode.Create, FileAccess.Write);
+            fsInput.Seek(16 + 16, SeekOrigin.Begin);  // Skip the first 32 bytes (Salt and header)
+
+            byte[] encryptedPage = new byte[1024 + 20 + 16];  // Page size + HMAC + IV
+            byte[] decryptedPage;
+
+            int pageNum = 1;
+            while (fsInput.Read(encryptedPage, 0, encryptedPage.Length) > 0)
+            {
+                decryptedPage = CryptoHelper.DecryptPage(encryptedPage, encryptionKey, hmacKey, pageNum);
+                fsOutput.Write(decryptedPage, 0, decryptedPage.Length);
+                pageNum++;
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Usage: SQLCipherDecryptor <input_file_path> <output_file_path> <password>");
+                    return;
+                }
+
+                string inputFilePath = args[0];
+                string outputFilePath = args[1];
+                string password = args[2];
+
+                await Decrypt(inputFilePath, outputFilePath, password);
+
+                Console.WriteLine("Decryption completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.ToString()}");
+            }
+        }
+    }
+}
